a156ab163d0efa0d39d0b46c3b9dac4fdd7141ba
Saqoosha <a@saqoo.sh>
2023-06-14 14:43:02 +0900

    Revert "gpt-3.5-turbo-0613"
    This reverts commit b52879fac4decc1c640644c066f812e9aedff1ee.

--- src/chat.ts
+++ src/chat.ts
@@ -14,7 +14,7 @@ export type ChatMessage = {
 
 export async function createChatCompletion(messages: ChatMessage[]) {
     const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0613",
+        model: "gpt-3.5-turbo",
         messages,
     });
     return completion.data.choices[0].message?.content;
@@ -22,7 +22,7 @@ export async function createChatCompletion(messages: ChatMessage[]) {
 
 export async function createChatCompletionStream(messages: ChatMessage[]) {
     const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0613",
+        model: "gpt-3.5-turbo",
         messages,
         stream: true,
     }, { responseType: 'stream' });
